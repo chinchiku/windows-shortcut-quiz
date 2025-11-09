d75451a1d7fad34b1e2452b83a35b1e72b23b073
chinchiku <rinmiya1007@gmail.com>
2025-11-09 15:51:30 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+// data.js の中身
+
+const shortcutData = {
+  
+  // Google Chrome のデータ
+  chrome: [
+    { q: "新しいタブを開く", a: "Ctrl + T" },
+    { q: "現在のタブを閉じる", a: "Ctrl + W" },
+    { q: "閉じたタブを開き直す", a: "Ctrl + Shift + T" },
+    { q: "アドレスバーに移動", a: "Ctrl + L" },
+    { q: "ページ内検索", a: "Ctrl + F" },
+    { q: "次のタブに移動", a: "Ctrl + Tab" },
+    { q: "前のタブに移動", a: "Ctrl + Shift + Tab" }
+  ],
+
+  // Google スライド のデータ
+  slides: [
+    { q: "新しいスライド", a: "Ctrl + M" },
+    { q: "スライドを複製", a: "Ctrl + D" },
+    { q: "テキストを中央揃え", a: "Ctrl + Shift + E" },
+    { q: "テキストを左揃え", a: "Ctrl + Shift + L" },
+    { q: "太字", a: "Ctrl + B" },
+    { q: "元に戻す", a: "Ctrl + Z" },
+    { q: "やり直す", a: "Ctrl + Y" }
+  ],
+
+  // Google スプレッドシート のデータ
+  sheets: [
+    { q: "元に戻す", a: "Ctrl + Z" },
+    { q: "やり直す", a: "Ctrl + Y" },
+    { q: "コピー", a: "Ctrl + C" },
+    { q: "貼り付け", a: "Ctrl + V" },
+    { q: "値のみ貼り付け", a: "Ctrl + Shift + V" },
+    { q: "行全体を選択", a: "Shift + Space" },
+    { q: "列全体を選択", a: "Ctrl + Space" }
+  ]
+};
